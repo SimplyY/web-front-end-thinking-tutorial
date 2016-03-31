a98dd391fbb493414cff440bb51204757540fed0
slience2014 <793919316@qq.com>
2016-03-31 21:06:17 +0800

    错的

---
+++ practice/machi/1/anwser.js
@@ -0,0 +1,16 @@
+/**
+ * Created by j on 2016/3/31.
+ */
+function spacify(item){
+
+    it.split(item);
+    var a;
+   for(var a=0;a<it.length;a+2)
+   {
+       it.push()
+   }
+    return it;
+}
+var arr="hello world";
+var a=spacify(arr);
+console.log(a);
