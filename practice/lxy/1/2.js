f669e1292f871ef3b61e4f7f3f6e0dcc92674fd6
Lxinyuelxy <836032851@qq.com>
2016-04-03 19:59:01 +0800

    finish lxy-2,4,5

--- practice/lxy/1/2.js
+++ practice/lxy/1/2.js
@@ -3,5 +3,7 @@
 /**
  * Created by Administrator on 2016/3/31.
  */
-var arry=new Array('1','2');
-console.log((Number("123")));
+var arry1=['1','2'];
+var arry2=arry1.map(Number);
+
+console.log(arry2);
---
+++ practice/lxy/1/5.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/4/3.
+ */
+    function change(aa) {
+    var bb=Array.from(aa) // string to arry
+
+    cc=bb.sort()  //对arry排序
+
+    dd=cc.join('') //arry to string
+
+    console.log(dd)
+}
+
+change('cdbae')
