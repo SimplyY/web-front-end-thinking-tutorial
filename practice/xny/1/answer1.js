c5ae37d74c6c168782dd1f16bcb678832639061a
Khalil <578755492@qq.com>
2016-04-06 22:15:57 +0800

    second homework from 谢柠宇 and 刘家旺

--- practice/xny/1/answer1.js
+++ practice/xny/1/answer1.js
@@ -1,6 +1,3 @@
-/**
- * Created by admin on 2016/9/6.
- */
 var str1 = "123"
 var answer1 = parseInt(str1)
 console.log(str1)
---
+++ practice/xny/1/try.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Khalil on 2016/4/4.
+ */
+var array = ['1','2','1','3','4']
+var answer2 = array.map(Number)
+console.log(array);
+console.log(answer2);
