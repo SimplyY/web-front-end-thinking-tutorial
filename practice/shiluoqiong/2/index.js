d58df493b4cb128842c7c4a85bdc510bd0dc3623
dai <609566704@qq.com>
2016-04-08 17:34:55 +0800

    作业2：搜索框的自动补全，鼠标和键盘响应

--- practice/shiluoqiong/2/index.js
+++ practice/shiluoqiong/2/index.js
@@ -1,108 +1,130 @@
-/**
- * Created by luoqiong on 2016/4/7.
- */
-(function(){
-    //获取div search 和 input
+(function() {
     var search = document.getElementById('search');
-    var inputNode = document.getElementsByTagName('input')[0];
-    //新建ul，用来放自动补全的字符串，向search添加ul结点
     var ul = document.createElement('ul');
     search.appendChild(ul);
-    var data = ['ab','bc','abc','bcd','cde','daidaidai','dyyre'];
+
+    var inputNode = document.getElementsByTagName('input')[0];
+    var data = ['ab', 'abe', 'ansjka', 'bc', 'bgs', 'cs', 'dw', 'eac', 'fg', 'gw','daidaidai'];
 
     listenChange();
 
-    //监听变化
+    //listening the change of input
     function listenChange(){
         if(inputNode.addEventListener){
             inputNode.addEventListener('input', handler, false);
-        }else if(inputNode.attachEvent) {
-            //IE
+        }else if(inputNode.attachEvent){
+            //IE8及以下
             inputNode.attachEvent('onpropertychange', handler);
         }
     }
 
-    //若监听到输入框有变化，则调用回调函数handler
-    function handler() {
-        //获得输入框中的字符串
-        var inputStr = inputNode.value;
-        if(!inputStr) {
-            clearBox();
+    //if the inputText change, call the callback function
+    function handler(){
+        var inputVal = (inputNode.value).trim();
+        if(!inputVal){
+            clear();
             return;
         }
-        //每一次改变都把原来匹配的结果数组删除
-        //然后根据现在的输入自动匹配
-        clearBox();
-        autoSuggest(inputStr);
+        clear();
+        autoSuggest(inputVal);
         replaceContent(ul);
+    }
+
+    function autoSuggest(value){
+        var listNode;
+        var list = findResults(value);
+
+        for (var i = 0, len = list.length; i < len; i++) {
+            listNode = document.createElement('li');
+            listNode.className= "li";
+            listNode.innerHTML = list[i];
+            ul.appendChild(listNode);
+        }
     }
 
     function replaceContent(ul){
         //click
+        delegate(ul, 'li', 'click', function(){
+            //this.innerHTML获取target的内容
+            inputNode.value = this.innerHTML;
+            //after click,remove list
+            clear();
+        });
 
-        //keyDowm
-        keydown();
+        //keydown
+        keyMove();
     }
 
-    //将匹配到的字符串添加到ul中   
-    function autoSuggest(value){
-        var liNode;
-        var list = stringMatch(value);
+    function delegate(parent, child, eventName, handler){
+        var childNode = parent.querySelectorAll(child);
+        var childArr = Array.prototype.slice.call(childNode);
 
-        for(let i = 0; i < list.length; i++) {
-            liNode = document.createElement('li');
-            liNode.className = "li";
-            liNode.innerHTML = list[i];
-            ul.appendChild(liNode);
-        }
+        parent.addEventListener(eventName, function(e){
+            e = e || window.event;
+            var eventTarget = e.target || e.srcElement;
+
+            if(childArr.indexOf(eventTarget) !== -1){
+                handler.call(eventTarget, e);
+            }
+        }, false);
     }
 
-    //根据每次的值匹配结果并返回
-    function stringMatch(value) {
-        var result = new RegExp('^' + value);
-        var list = [];
-        for(let i = 0; i < data.length; i++)
-            if(result.test(data[i]))
-                list.push(data[i]);
-        return list;
-    }
-
-    function clearBox() {
-        var listNode = ul.getElementsByTagName('li');
-        for(let i = listNode.length - 1; i >= 0; i--)
-            ul.removeChild(listNode[i]);
-    }
-
-    function keydown(){
+    function keyMove(){
         document.onkeydown = function(event){
             event = event || window.event;
             var currentLi = document.getElementsByClassName('active')[0];
 
-            //up
-            if(event.keyCode == 38){
+            //向上
+            if(event.keyCode === 38){
                 if(currentLi){
                     var preLi = currentLi.previousSibling;
-                    if(preLi) {
-                        removeClass(preLi,'active');
-                        addClass(currentLi,'active');
+                    if(preLi){
+                        removeClass(currentLi, 'active');
+                        addClass(preLi, 'active');
                     }
                 }
             }
-            //dowm
-            if(evnet.keyCode == 40){
-                if(currentLi){
+            //向下
+            if(event.keyCode === 40){
+                if(!currentLi){
+                    currentLi = ul.getElementsByTagName('li')[0];
+                    addClass(currentLi, 'active');
+                }else{
                     var nextLi = currentLi.nextSibling;
                     if(nextLi){
-                        removeClass(currentLi,'active');
-                        addClass(nextLi,'active');
+                        removeClass(currentLi, 'active');
+                        addClass(nextLi, 'active');
                     }
                 }
+
             }
             //enter
-            if(event.keyCode == 13){
+            if(event.keyCode === 13){
                 inputNode.value = currentLi.innerHTML;
-                clearBox();
+                clear();
             }
+        };
+    }
+
+    //get match results
+    function findResults(value){
+        var re = new RegExp('^' + value);
+        var list = [];
+
+        for (var i = 0, len = data.length; i < len; i++) {
+            if(re.test(data[i])){
+                list.push(data[i]);
+            }
+        }
+
+        return list;
+    }
+
+    function clear(){
+        var listNode = ul.getElementsByClassName('li');
+
+        for (var len = listNode.length, i = len - 1; i >= 0; i--) {
+            ul.removeChild(listNode[i]);
         }
     }
 
