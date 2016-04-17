41cf8f5781b0872dd47cb43057054aa0a48e4729
长凄 <2026929156@qq.com>
2016-04-17 19:27:56 +0800

    finish todolist

--- practice/yuwei/3/src/component/TodoList.jsx
+++ practice/yuwei/3/src/component/TodoList.jsx
@@ -3,21 +3,35 @@ var classNames = require('classnames');
 
 class TodoList extends Component {
     constructor(props) {
-        super(props)
+        super(props),
+        this.state = {
+            editedIndex: -1,
+            isActive: false
+        }
     }
 
     render() {
+        const { editedIndex } = this.state;
+
+        let itemClasses = this.props.items.map((item, index) => {
+            let itemClass = classNames({
+                'itemText': true,
+                'active': index === parseInt(editedIndex, 10)
+            });
+            return itemClass;
+        })
+
         return (
             <ul>
                 {this.props.items.map((item, i) => {
                     return (
                         //给子级设置唯一的标识key
-                        <li className="item" key={i} onDoubleClick={this.doubleClick.bind(this)}>
+                        <li className="item" key={i} data-index={i} onDoubleClick={this.doubleClick.bind(this)}>
 
                             <label>{item}</label>
                             <button className="delete" onClick={this.deleteItem.bind(this, i)}></button>
 
-                            <input className="itemText" value={item}
+                            <input className={itemClasses[i]} value={item}
                                 onChange={this.itemTextChange.bind(this)}
                                 onKeyDown={this.keyDownChange.bind(this)}
                                 >
@@ -39,22 +53,25 @@ class TodoList extends Component {
     }
 
     doubleClick(e){
-        var inputNode = e.target.getElementsByTagName('input')[0];
-        var newClassName = classNames('itemText', 'active');
-        inputNode.className = newClassName;
+        this.setState({
+            editedIndex: e.target.dataset.index,
+            isActive: true
+        });
     }
 
     itemTextChange(e){
         var newText = e.target.value.trim();
-        console.log(newText);
-        // this.props.items[index] = newText;
+        var index = this.state.editedIndex;
+        this.props.items[index] = newText;
         this.props.changeItems(this.props.items);
     }
 
     keyDownChange(e){
         if(e.keyCode === 13){
-            var newClassName = classNames('itemText');
-            e.target.className = newClassName;
+            this.setState({
+                editedIndex: -1,
+                isActive: false
+            });
         }
     }
 }
